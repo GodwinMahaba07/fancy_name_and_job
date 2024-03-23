fdc4a21410a461e1be1151ce62a9f35e9c524e91
Your Name <mahabagodwinanton@gmail.com>
2024-03-23 23:00:38 +0800

    Define the text color,Lines and text

--- #Pseudocode.py
+++ #Pseudocode.py
@@ -1,5 +1,17 @@
 #Pseudocode 
 
+def print_fancy(text, color_code):
+    fancy_text = ""
+    for char in text:
+        fancy_text += char.upper() + " "
+
+    line_length = len(fancy_text) + 4  
+    line = "\033[{}m{}\033[0m".format(color_code, "-" * line_length)  
+    print(line)
+    print("\033[{}m  {}  \033[0m".format(color_code, fancy_text))  
+    print(line)
+
+
 #input name,age,address and dream job
 
 
@@ -12,9 +24,10 @@ dream_job = input("What is your dream job?: ")
 #print all 
 
 print("Name:", name)
-print("Age:", age)
+print("Age:" , age)
 print("Address:", address)
 print("Dream Job:", dream_job)
 
 #print all with fancy text
+
 
