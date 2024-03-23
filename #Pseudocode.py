f27b8bfed078856d7c81c970f9d99bbd5a5f8a10
Your Name <mahabagodwinanton@gmail.com>
2024-03-23 22:39:08 +0800

    Print all

--- #Pseudocode.py
+++ #Pseudocode.py
@@ -1,10 +1,20 @@
 #Pseudocode 
 
 #input name,age,address and dream job
+
+
 name = input("What is your name?: ")
 age = input("What is your age?: ")
 address = input ("What is your address?: ")
 dream_job = input("What is your dream job?: ")
+
+
 #print all 
+
+print("Name:", name)
+print("Age:", age)
+print("Address:", address)
+print("Dream Job:", dream_job)
+
 #print all with fancy text
 
